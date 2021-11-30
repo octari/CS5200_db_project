e856f46bef46337b73e90c8388c2e640d81029ad
lisa-lam13 <lisa.lam5169@gmail.com>
2021-11-30 17:06:38 -0500

    rearranged classes codes

--- src/main/webapp/react/social/index.js
+++ src/main/webapp/react/social/index.js
@@ -2,37 +2,16 @@
 import CompanyList from "./companies/company-list";
 import CompanyFormEditor from "./companies/company-form-editor";
 
-// import ApplicantList from "./applicants/applicant-list";
-// import ApplicantFormEditor from "./applicants/applicant-form-editor";
-import JobList from "./jobs/job-list";
-import JobFormEditor from "./jobs/job-form-editor";
-
 const {HashRouter, Route} = window.ReactRouterDOM; 
 const App = () => {
     return (
         <div className="container-fluid">
-            {/*<HashRouter>*/}
-            {/*    <Route path={["/applicants", "/"]} exact={true}>*/}
-            {/*        <ApplicantList/>*/}
-            {/*    </Route>*/}
-            {/*    <Route path="/applicants/:id" exact={true}>*/}
-            {/*        <ApplicantFormEditor/>*/}
-            {/*    </Route>*/}
-            {/*</HashRouter>*/}
             <HashRouter>
-
                 <Route path={["/companies", "/"]} exact={true}>
                     <CompanyList/>
                 </Route>
                 <Route path="/companies/:id" exact={true}>
                     <CompanyFormEditor/>
-
-                <Route path={["/jobs", "/"]} exact={true}>
-                    <JobList/>
-                </Route>
-                <Route path="/jobs/:id" exact={true}>
-                    <JobFormEditor/>
-
                 </Route>
             </HashRouter>
         </div>
