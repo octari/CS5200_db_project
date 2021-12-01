70d33d75b661b8dfc0dcff35a7a29a3b8d51b8cd
octari <ycjariel@gmail.com>
2021-11-30 19:37:42 -0500

    showing Applicant, Job, Company List

--- src/main/webapp/react/social/index.js
+++ src/main/webapp/react/social/index.js
@@ -1,3 +1,7 @@
+import ApplicantList from "./applicants/applicant-list";
+import ApplicantFormEditor from "./applicants/applicant-form-editor";
+import JobList from "./jobs/job-list";
+import JobFormEditor from "./jobs/job-form-editor";
 import CompanyList from "./companies/company-list";
 import CompanyFormEditor from "./companies/company-form-editor";
 
@@ -6,6 +10,22 @@ const {HashRouter, Route} = window.ReactRouterDOM;
 const App = () => {
     return (
         <div className="container-fluid">
+            <HashRouter>
+                <Route path={["/applicants", "/"]} exact={true}>
+                    <ApplicantList/>
+                </Route>
+                <Route path="/applicants/:id" exact={true}>
+                    <ApplicantFormEditor/>
+                </Route>
+            </HashRouter>
+            <HashRouter>
+                <Route path={["/jobs", "/"]} exact={true}>
+                    <JobList/>
+                </Route>
+                <Route path="/jobs/:id" exact={true}>
+                    <JobFormEditor/>
+                </Route>
+            </HashRouter>
             <HashRouter>
                 <Route path={["/companies", "/"]} exact={true}>
                     <CompanyList/>
